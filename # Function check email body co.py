f23e825b2d3c8e769acfbc5ce2189de412d5595f
Syed Hussain Abid <33356@students.riphah.edu.pk>
2025-05-06 02:14:13 +0500

    Add files via upload

---
+++ # Function check email body co.py
@@ -0,0 +1,4 @@
+# Function to check if the email body contains links
+def extract_links(body):
+    url_pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    return re.findall(url_pattern, body)
